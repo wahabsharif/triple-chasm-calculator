01871d8e851a5375514b92cb91a86a3936e0a6be
Muhammad Wahab Sharif <www.rex19@gmail.com>
2025-07-14 16:50:18 +0500

    minor updates

--- resources/views/questionnaire.blade.php
+++ resources/views/questionnaire.blade.php
@@ -50,19 +50,6 @@
         .questionnaire-table td[rowspan] {
             font-weight: bold;
         }
-
-        .alert {
-            padding: 15px;
-            margin-bottom: 20px;
-            border: 1px solid transparent;
-            border-radius: 4px;
-        }
-
-        .alert-success {
-            color: #3c763d;
-            background-color: #dff0d8;
-            border-color: #d6e9c6;
-        }
     </style>
 
     <div class="container questionnaire-container">
